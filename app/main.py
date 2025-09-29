14628a4eae27440de8d6a03b3295a7b97596292a
Radoslaw Ptak <radek1026@interi.pl>
2025-09-29 19:21:02 +0200

    CR in implement py-shop-trip

--- app/main.py
+++ app/main.py
@@ -14,29 +14,36 @@ def shop_trip() -> None:
         for shop in config["shops"]
     ]
     customer = Customer(
-        name=config["customer"]["name"],
-        location=config["customer"]["location"],
-        money=config["customer"]["money"],
-        product_cart=config["customer"]["product_cart"],
-        car_data=config["customer"]["car_data"]
+        name=config["customers"][0]["name"],
+        location=config["customers"][0]["location"],
+        money=config["customers"][0]["money"],
+        product_cart=config["customers"][0]["product_cart"],
+        car_data=config["customers"][0]["car"]
     )
-    fuel_price = config["fuel_price"]
+    fuel_price = config["FUEL_PRICE"]
 
     cheapest_trip = None
     best_shop = None
 
     for shop in shops:
-        trip_cost = customer.calculate_trip_cost(shop, fuel_price)
-        if trip_cost is not None and customer.money >= trip_cost:
-            if cheapest_trip is None or trip_cost < cheapest_trip:
-                cheapest_trip = trip_cost
+        trip_cost = (
+            customer.calculate_trip_cost(shop, fuel_price))
+        product_total = (
+            shop.calculate_products_cost(customer.product_cart))
+        total_cost = trip_cost + product_total if (
+                trip_cost is not None) else None
+
+        if total_cost is not None and customer.money >= total_cost:
+            if cheapest_trip is None or total_cost < cheapest_trip:
+                cheapest_trip = total_cost
                 best_shop = shop
 
     if best_shop:
+        trip_cost = customer.calculate_trip_cost(best_shop, fuel_price)
         product_total = best_shop.calculate_products_cost(
             customer.product_cart
         )
-        customer.make_purchase(best_shop, cheapest_trip, product_total)
+        customer.make_purchase(best_shop, trip_cost, product_total)
     else:
         print(
             f"{customer.name} doesn't have enough money"
